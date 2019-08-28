d98c3ca18c6d829d88b851566badae5ddbf9e821
Jay Díaz Santana <jaydsxd@gmail.com>
2019-08-27 22:30:42 -0400

    Updates

---
+++ precache-manifest.d5c05b736696502848347cb3f0fe3d36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e0911d475895aa25d38a3a138b9b5b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "64f38c79a4573b6effc4",
+    "url": "/monster-rolodex/static/css/main.e76fb680.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monster-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "64f38c79a4573b6effc4",
+    "url": "/monster-rolodex/static/js/main.da642000.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
